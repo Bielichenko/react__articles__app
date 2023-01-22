a6b6344b3256d096813bc176175d41f54aae485f
Illia Bielichenko <ibelich12@gmail.com>
2023-01-22 17:48:13 +0200

    added styles to selectedArticle component

--- src/components/SelectedArticle/SelectedArticle.tsx
+++ src/components/SelectedArticle/SelectedArticle.tsx
@@ -42,9 +42,63 @@ export const SelectedArticle = () => {
   return (
     selectedArticle
       ? (
-        <article>
-          {selectedArticle.title}
-          <Link to="/">Back to homepage</Link>
+        <article className="selectedArticle">
+          <div className="selectedArticle__backImage" style={{ backgroundImage: `url(${selectedArticle.imageUrl})` }}>
+          </div>
+          <main className="selectedArticle__textContent">
+            <h1 className="selectedArticle__title">
+              {selectedArticle.title}
+            </h1>
+            <p className="selectedArticle__textParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum ornare convallis
+              non etiam tincidunt tincidunt. Non dolor vel purus id. Blandit habitasse volutpat
+              id dolor pretium, sem iaculis. Faucibus commodo mauris enim, turpis blandit.
+              Porttitor facilisi viverra mi lacus lacinia accumsan. Pellentesque gravida ligula
+              bibendum aliquet nulla massa elit. Ac faucibus donec sit morbi pharetra urna. Vel
+              facilisis amet placerat ultrices lobortis proin nulla. Molestie tellus sed
+              pellentesque tortor vitae eu cras nisl. Sem facilisi amet vitae ultrices nullam
+              tellus. Pellentesque eget iaculis morbi at quis eget lacus, aliquam etiam. Neque
+              ipsum, placerat vel convallis nulla orci, nunc etiam. Elementum risus facilisi
+              mauris diam amet et sed.
+            </p>
+            <p className="selectedArticle__textParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum ornare convallis
+              non etiam tincidunt tincidunt. Non dolor vel purus id. Blandit habitasse volutpat
+              id dolor pretium, sem iaculis. Faucibus commodo mauris enim, turpis blandit.
+              Porttitor facilisi viverra mi lacus lacinia accumsan. Pellentesque gravida ligula
+              bibendum aliquet nulla massa elit. Ac faucibus donec sit morbi pharetra urna. Vel
+              facilisis amet placerat ultrices lobortis proin nulla. Molestie tellus sed
+              pellentesque tortor vitae eu cras nisl. Sem facilisi amet vitae ultrices nullam
+              tellus. Pellentesque eget iaculis morbi at quis eget lacus, aliquam etiam. Neque
+              ipsum, placerat vel convallis nulla orci, nunc etiam. Elementum risus facilisi
+              mauris diam amet et sed.
+            </p>
+            <p className="selectedArticle__textParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum ornare convallis
+              non etiam tincidunt tincidunt. Non dolor vel purus id. Blandit habitasse volutpat
+              id dolor pretium, sem iaculis. Faucibus commodo mauris enim, turpis blandit.
+              Porttitor facilisi viverra mi lacus lacinia accumsan. Pellentesque gravida ligula
+              bibendum aliquet nulla massa elit. Ac faucibus donec sit morbi pharetra urna. Vel
+              facilisis amet placerat ultrices lobortis proin nulla. Molestie tellus sed
+              pellentesque tortor vitae eu cras nisl. Sem facilisi amet vitae ultrices nullam
+              tellus. Pellentesque eget iaculis morbi at quis eget lacus, aliquam etiam. Neque
+              ipsum, placerat vel convallis nulla orci, nunc etiam. Elementum risus facilisi
+              mauris diam amet et sed.
+            </p>
+            <p className="selectedArticle__textParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum ornare convallis
+              non etiam tincidunt tincidunt. Non dolor vel purus id. Blandit habitasse volutpat
+              id dolor pretium, sem iaculis. Faucibus commodo mauris enim, turpis blandit.
+              Porttitor facilisi viverra mi lacus lacinia accumsan. Pellentesque gravida ligula
+              bibendum aliquet nulla massa elit. Ac faucibus donec sit morbi pharetra urna. Vel
+              facilisis amet placerat ultrices lobortis proin nulla. Molestie tellus sed
+              pellentesque tortor vitae eu cras nisl. Sem facilisi amet vitae ultrices nullam
+              tellus. Pellentesque eget iaculis morbi at quis eget lacus, aliquam etiam. Neque
+              ipsum, placerat vel convallis nulla orci, nunc etiam. Elementum risus facilisi
+              mauris diam amet et sed.
+            </p>
+          </main>
+          <Link to="/" className="selectedArticle__link">Back to homepage</Link>
         </article>
       )
       : <Cirlce color="#1976d2" />
